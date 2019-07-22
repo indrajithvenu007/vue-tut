1f7d4ce8b1581b1d3193d16382d73e7ee20de63b
indrajithvenu007 <indrajithvenu@gmail.com>
2019-07-22 19:43:02 +0530

    updated register and login to navigate to each other

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,37 @@
 <template>
     <div>
         <h1>Login</h1>
+        <div>
+            <input type="text" v-model="username" placeholder="username"/>
+            <br>
+            <input type="password" v-model="password" placeholder="password"/>
+            <br>
+            <button @click="doLogin">Login</button>
+            <router-link to="/register">
+                <a>Dont have an account?</a>
+            </router-link>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'login'
+    name: 'login',
+    data() {
+        return {
+            username: '',
+            password: ''
+        }
+    },
+    methods: {
+        doLogin() {
+            const payload = {
+                username: this.username,
+                password: this.password
+            }
+            console.log(payload);
+        }
+    }
 }
 </script>
 
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,12 +1,37 @@
 <template>
     <div>
         <h1>Register</h1>
+        <div>
+            <input type="text" v-model="username" placeholder="username"/>
+            <br>
+            <input type="password" v-model="password" placeholder="password"/>
+            <br>
+            <button @click="doRegister">Register</button>
+            <router-link to="/login">
+                <a>Already registered?</a>
+            </router-link>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'register'
+    name: 'register',
+    data() {
+        return {
+            username: '',
+            password: ''
+        }
+    },
+    methods: {
+        doRegister() {
+            const payload = {
+                username: this.username,
+                password: this.password
+            }
+            console.log(payload);
+        }
+    }
 }
 </script>
 
